b14c2333f364a9290952467c7a288a49ffd394a3
Jacob Massey <jacob@jacobmassey.com>
2018-04-20 23:52:25 -0400

    add some aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,4 +13,14 @@ alias ln='nocorrect ln -i'
 alias mv='nocorrect mv -i'
 alias md='nocorrect md'
 
-alias rm="trash"
+alias rm='trash'
+
+alias d='docker'
+alias v='vim'
+alias vi='vim'
+alias g='git'
+alias ga='git add'
+alias gs='git status'
+alias gd='git diff'
+alias gc='git commit -m'
+alias gp='git push'
